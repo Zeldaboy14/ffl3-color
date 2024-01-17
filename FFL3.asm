17ef799d449e8a460b27aaaf169c6b5d79ced40b
Adam Rippon <rippon@gmail.com>
2024-01-17 16:55:58 -0700

    Found free space and documented it.

--- FFL3.asm
+++ FFL3.asm
@@ -18,7 +18,16 @@
 
 .BACKGROUND "Final Fantasy Legend III (USA).gb"        ; This loads the ROM so we can write directly into it
 .UNBACKGROUND $3E70 $3FFF       ; Free space in bank $00
-.UNBACKGROUND $2FF73 $2FFFF     ; Free space in bank $0B
+.UNBACKGROUND $FF14 $FFFF       ; Free space in bank $03
+.UNBACKGROUND $1BFD2 $1BFFF     ; Free space in bank $06
+.UNBACKGROUND $1FFF0 $1FFFF     ; Free space in bank $07
+.UNBACKGROUND $23FB2 $23FFF     ; Free space in bank $08
+.UNBACKGROUND $27E98 $27FFF     ; Free space in bank $09
+.UNBACKGROUND $2BD9A $2BFFF     ; Free space in bank $0A
+.UNBACKGROUND $2FF74 $2FFFF     ; Free space in bank $0B
+.UNBACKGROUND $33FB2 $33FFF     ; Free space in bank $0C
+.UNBACKGROUND $37FE8 $37FFF     ; Free space in bank $0D
+.UNBACKGROUND $3BF66 $3BFFF     ; Free space in bank $0E
 .UNBACKGROUND $3FFCE $3FFFF     ; Free space in bank $0F
 ; ...
 
